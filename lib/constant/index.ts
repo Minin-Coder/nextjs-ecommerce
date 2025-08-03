5b5006559e4de1971b1675cd1cdf88c02f908a78
Farasat Ali <i.farasatrehmat@gmail.com>
2025-08-03 05:10:57 +0500

    header and footer starts

---
+++ lib/constant/index.ts
@@ -0,0 +1,9 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Prostore';
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'A modern e-commerce platform bult with Next.js';
+let url = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
+try {
+  url = new URL(url).toString();
+} catch {
+  url = 'http://localhost:3000';
+}
+export const SERVER_URL = url;
