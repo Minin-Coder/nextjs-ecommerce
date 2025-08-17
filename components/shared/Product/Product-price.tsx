48e70d7c3d827ce54a63becba66b6351d43b67cd
Farasat Ali <i.farasatrehmat@gmail.com>
2025-08-17 16:33:32 -0700

    responsivenes done, working on products

---
+++ components/shared/Product/Product-price.tsx
@@ -0,0 +1,9 @@
+const ProductPrice
+ = ({ value, className } : { value: number; className? : string}) => {
+    // ensure two decimal places
+    const stringValue = value.toFixed(2);
+    return (  );
+}
+ 
+export default ProductPrice
+;
