63c0b831c2f6eafa34c39f8e8557b46379fb6ece
Farasat Ali <i.farasatrehmat@gmail.com>
2025-09-07 04:36:37 +0500

    details page done

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -19,9 +19,42 @@ const ProductDetailsPage = async (props: {
           {/*Images Column*/}
           <div className="col-span-2">{/*Images component*/}</div>
           <div className="col-span-2 p-5">
-            <div className="flex flex-col gap-6">
-                
+            <div className="flex flex-col gap-5">
+                <p className="m-0">
+                    {product.brand} {product.category}
+                </p>
+                <h1 className="h3-bold m-0">{product.name}</h1>
+                <p className="m-0">{product.rating} of {product.numReviews} Reviews</p>
+                <div className="flex flex-col sm:flex-row sm:items-center gap-3">
+                    <ProductPrice value={Number(product.price)} className="w-24 rounded-full bg-green-100 text-green-700 px-5 py-2 text-center" />
+                </div>
             </div>
+            <div className="mt-10">
+                <p className="font-semibold">Description:</p>
+                <p>{product.description}</p>
+            </div>
+          </div>
+          {/*Action Column*/}
+          <div>
+            <Card >
+                <CardContent className="p-4" > 
+                    <div className="mb-2 flex justify-between">
+                        <div>Price</div>
+                        <div>
+                            <ProductPrice  value={Number(product.price)}/>
+                        </div>
+                    </div>
+                    <div className="mb-2 flex justify-between">
+                        <div>Status</div>
+                        {product.stock > 0 ? (
+                           <Badge variant="outline">In Stock</Badge> 
+                        ) : (
+                            <Badge variant="destructive">Out of Stock</Badge>
+                        )}
+
+                    </div>
+                </CardContent>
+            </Card>
           </div>
         </div>
       </section>
