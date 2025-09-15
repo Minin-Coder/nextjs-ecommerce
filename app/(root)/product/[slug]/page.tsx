099fca21ff3a1cd3fdb4d7dff64e4b9465620c29
Farasat Ali <i.farasatrehmat@gmail.com>
2025-09-15 22:47:32 +0500

    .env errors fixed

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -24,49 +24,58 @@ const ProductDetailsPage = async (props: {
           </div>
           <div className="col-span-2 p-5">
             <div className="flex flex-col gap-5">
-                <p className="m-0">
-                    {product.brand} {product.category}
-                </p>
-                <h1 className="h3-bold m-0">{product.name}</h1>
-                <p className="m-0">{product.rating} of {product.numReviews} Reviews</p>
-                <div className="flex flex-col sm:flex-row sm:items-center gap-3">
-                    <ProductPrice value={Number(product.price)} className="w-24 rounded-full bg-green-100 text-green-700 px-5 py-2 text-center" />
-                </div>
+              <p className="m-0">
+                {product.brand} {product.category}
+              </p>
+              <h1 className="h3-bold m-0">{product.name}</h1>
+              <p className="m-0">
+                {product.rating} of {product.numReviews} Reviews
+              </p>
+              <div className="flex flex-col sm:flex-row sm:items-center gap-3">
+                <ProductPrice
+                  value={Number(product.price)}
+                  className="w-24 rounded-full bg-green-100 text-green-700 px-5 py-2 text-center"
+                />
+              </div>
             </div>
             <div className="mt-10">
-                <p className="font-semibold">Description:</p>
-                <p>{product.description}</p>
+              <p className="font-semibold">Description:</p>
+              <p>{product.description}</p>
             </div>
           </div>
           {/*Action Column*/}
           <div>
-            <Card >
-                <CardContent className="p-4" > 
-                    <div className="mb-2 flex justify-between">
-                        <div>Price</div>
-                        <div>
-                             <ProductPrice  value={Number(product.price)}/>
-                        </div>
-                    </div>
-                    <div className="mb-2 flex justify-between">
-                        <div>Status</div>
-                        {product.stock > 0 ? (
-                           <Badge variant="outline">&nbsp;&nbsp;In Stock&nbsp;&nbsp;</Badge> 
-                        ) : (
-                            <Badge variant="destructive" className="badge-destructive">&nbsp;&nbsp;Out of Stock&nbsp;&nbsp;</Badge>
-                        )}
-
-                    </div>
-                    {product.stock > 0 && (
-                      <div className="flex-center">
-                        <Button className="w-100 btn-dark mt-2">Add To Cart</Button>
-                      </div>
-                    ) }
-                </CardContent>
+            <Card>
+              <CardContent className="p-4">
+                <div className="mb-2 flex justify-between">
+                  <div>Price</div>
+                  <div>
+                    <ProductPrice value={Number(product.price)} />
+                  </div>
+                </div>
+                <div className="mb-2 flex justify-between">
+                  <div>Status</div>
+                  {product.stock > 0 ? (
+                    <Badge variant="outline">
+                      &nbsp;&nbsp;In Stock&nbsp;&nbsp;
+                    </Badge>
+                  ) : (
+                    <Badge variant="destructive" className="badge-destructive">
+                      &nbsp;&nbsp;Out of Stock&nbsp;&nbsp;
+                    </Badge>
+                  )}
+                </div>
+                {product.stock > 0 && (
+                  <div className="flex-center">
+                    <Button className="w-100 btn-dark mt-2">Add To Cart</Button>
+                  </div>
+                )}
+              </CardContent>
             </Card>
           </div>
         </div>
       </section>
+      
     </>
   );
 };
